96f15f2e21c4b1a66143cc397e6853a8a31a414a
Grinadir <zzzania683@gmail.ru>
2022-07-21 17:36:40 +0300

    Change info.json and index.htm

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -2,39 +2,47 @@
 
 ESP8266WebServer webserver(80);
 
-void handleRoot(){
+void handleRoot()
+{
     SPIFFS.begin();
     Serial.println("\nSend 200 on root");
-    //webserver.send(200, "text/html", SPIFFS.open("/file.txt", "r"));
+    // webserver.send(200, "text/html", SPIFFS.open("/file.txt", "r"));
     File file = SPIFFS.open("/index.html", "r");
     size_t sent = webserver.streamFile(file, "text/html");
     file.close();
-
 }
 
-void handleCSS(){
+void handleCSS()
+{
     SPIFFS.begin();
-    Serial.println("\nSend 200 on root");
-    //webserver.send(200, "text/html", SPIFFS.open("/file.txt", "r"));
+    // webserver.send(200, "text/html", SPIFFS.open("/file.txt", "r"));
     File file = SPIFFS.open("/style.css", "r");
     size_t sent = webserver.streamFile(file, "text/css");
     file.close();
-
 }
 
+void handleJSON()
+{
+    SPIFFS.begin();
+    // webserver.send(200, "text/html", SPIFFS.open("/file.txt", "r"));
+    File file = SPIFFS.open("/info.json", "r");
+    size_t sent = webserver.streamFile(file, "text/json");
+    file.close();
+}
 
+void initWebServer()
+{
 
-
-void initWebServer(){
     
     webserver.on("/", handleRoot);
     webserver.on("/style.css", handleCSS);
+    webserver.on("/info.json", handleJSON);
     Serial.println("\nStart webserver");
     webserver.begin();
-
 }
 
-void handleClient(){
+void handleClient()
+{
     webserver.handleClient();
     delay(1);
 }
